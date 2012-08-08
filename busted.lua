e4b03e9dfb9f06d86c5f4ce01f6f35f60592503e
Jack Lawson <ajacksified@gmail.com>
2012-08-08 13:51:51 -0700

    Better error message output in preparation of asserts

--- busted.lua
+++ busted.lua
@@ -80,20 +80,22 @@ test = function(description, callback)
     linedefined = debug_info.linedefined,
   }
 
-  if pcall(callback) then
+  local status,err = pcall(callback)
+
+  if err then
+    if not busted_options.defer_print then
+      io.write(failure_string())
+      io.flush()
+    end
+
+    return { type = "failure", description = "description", info = info, trace = debug.traceback(), err = err }
+  else
     if not busted_options.defer_print then
       io.write(success_string())
       io.flush()
     end
 
     return { type = "success", description = "description", info = info }
-  else
-    if not busted_options.defer_print then
-      io.write(failure_string())
-      io.flush()
-    end
-
-    return { type = "failure", description = "description", info = info, trace = debug.traceback() }
   end
 end
 
@@ -165,7 +167,8 @@ error_description = function(status)
     return "\n\n"..ansicolors("%{red}Failure").." → "..
            ansicolors("%{blue}"..status.info.short_src).." @ "..
            ansicolors("%{blue}"..status.info.linedefined)..
-           "\n"..ansicolors("%{bright}"..status.description)
+           "\n"..ansicolors("%{bright}"..status.description)..
+           "\n"..status.err
   end
 
   return "\n\nFailure in block \""..status.description.."\"\n→ "..status.info.short_src.." @ "..status.info.linedefined
