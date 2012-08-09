32f83c2b27f18cd81c77b744a06357797c777146
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-08-09 12:22:03 -0700

    moved assertion functions out of assert.lua and into assertions.lua as a self executing plugin.

--- lib/assert.lua
+++ lib/assert.lua
@@ -1,6 +1,3 @@
-local util = require 'lib/util'
-old_assert = assert
-
 assert = {}
 assert.mod = true
 assert.assertions = {}
@@ -69,61 +66,6 @@ function assert.raw.none(list)
   error("NOT IMPLEMENTED")
 end
 
-local function has_error(func)
-  return not pcall(func)
-end
-
-local function unique(list, deep)
-  for k,v in pairs(list) do
-    for k2, v2 in pairs(list) do
-      if k ~= k2 then
-        if deep and util.deepcompare(v, v2, true) then
-          return false
-        else
-          if v == v2 then
-            return false
-          end
-        end
-      end
-    end
-  end
-  return true
-end
-
-local function equals(...)
-  local prev = nil
-  for k,v in pairs({...}) do
-    if prev ~= nil and prev ~= v then
-      return false
-    end
-    prev = v
-  end
-  return true
-end
-
-local function same(...)
-  local prev = nil
-  for k,v in pairs({...}) do
-    if prev ~= nil then
-
-      if type(prev) == 'table' and type(v) == 'table' then
-        if not util.deepcompare(prev, v, true) then
-          return false
-        end
-      else
-        if prev ~= v then
-          return false
-        end
-      end
-    end
-    prev = v
-  end
-  return true
-end
-
-assert:register("same", same, "These values are not the same")
-assert:register("equals", equals, "These values are not equal")
-assert:register("unique", unique, "These values are not unique")
-assert:register("error", has_error, "Expected error from function")
-
 assert=setmetatable(assert, assert.__meta)
+
+require 'lib/assertions'
