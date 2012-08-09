389858bbeb1ca5a0473d950015b54119e2a3cc67
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-08-09 12:32:51 -0700

    updated descriptions

--- spec/busted_assertions.lua
+++ spec/busted_assertions.lua
@@ -1,32 +1,32 @@
 describe("Test Assertions", function()
-  it("Tests Assert Same", function()
+  it("Checks to see if tables 1 and 2 are the same", function()
     local table1 = { derp = false}
     local table2 = { derp = false}
     assert.same(table1, table2)
   end)
 
-  it("Tests Assert equals", function()
+  it("Checks to see if tables 1 and 2 are equal", function()
     local table1 = { derp = false}
     local table2 = table1
     assert.equals(table1, table2)
   end)
 
-  it("Tests assert unique", function()
+  it("Checks to see if table1 only contains unique elements", function()
     local table2 = { derp = false}
     local table3 = { derp = true }
     local table1 = {table2,table3}
     assert.unique(table1, false)
   end)
 
-  it("Tests IS operator", function()
+  it("Ensures the is operator doesn't change the behavior of equals", function()
     assert.is.equals(true, true)
   end)
 
-  it("Tests NOT operator", function()
+  it("Ensures the not operator does change the behavior of equals", function()
     assert.isnot.equals(true, false)
   end)
 
-  it("Tests assert error", function()
+  it("Ensures that error only throws an error when the first argument function does not throw an error", function()
     assert.error(function() error("test") end)
   end)
 end)
