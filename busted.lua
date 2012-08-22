870d0dba84ec60c7682ea37b18a7b4654749335b
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-08-21 17:32:15 -0700

    refactor and cleanup

--- busted.lua
+++ busted.lua
@@ -1,181 +1,179 @@
 require 'luassert.all'
-
-local output = require 'output.utf_terminal'()
-
 local s = require 'say.s'
-s:set_namespace "en"
-
--- setup for stuff we use inside
 local global_context = { type = "describe", description = "global" }
-local current_context = global_context
-local busted_options = {}
 
--- Internal functions
-local split = function(string, sep)
-  local sep, fields = sep or ".", {}
-  local pattern = ("([^%s]+)"):format(sep)
-  string:gsub(pattern, function(c) fields[#fields+1] = c end)
-  return fields
-end
+local busted = {
+  root_context = global_context,
+  current_context = global_context,
+  output = require 'output.utf_terminal'(),
+  options = {},
 
-local function busted()
+  __call = function(self)
+    --setup options
+    s:set_namespace(self.options.lang)
+    self.output = require('output.'..self.options.output_lib)()
 
-  local function test(description, callback)
-    local debug_info = debug.getinfo(callback)
+    --run test
+    local function test(description, callback)
+      local debug_info = debug.getinfo(callback)
 
-    local info = {
-      source = debug_info.source,
-      short_src = debug_info.short_src,
-      linedefined = debug_info.linedefined,
-    }
+      local info = {
+        source = debug_info.source,
+        short_src = debug_info.short_src,
+        linedefined = debug_info.linedefined,
+      }
 
-    local stack_trace = ""
+      local stack_trace = ""
 
-    local function err_handler(err)
-      stack_trace = debug.traceback("", 4)
-      return err
+      local function err_handler(err)
+        stack_trace = debug.traceback("", 4)
+        return err
+      end
+
+      local status, err = xpcall(callback, err_handler)
+
+      local test_status = {}
+
+      if err then
+        test_status = { type = "failure", description = description, info = info, trace = stack_trace, err = err }
+      else
+        test_status = { type = "success", description = description, info = info }
+      end
+
+      if not self.options.defer_print then
+        self.output.currently_executing(test_status, self.options)
+      end
+
+      return test_status
     end
 
-    local status, err = xpcall(callback, err_handler)
+    --run test case
+    local function run_context(context)
+      local match = false
 
-    local test_status = {}
+      if self.options.tags and #self.options.tags > 0 then
+        for i,t in ipairs(self.options.tags) do
+          if context.description:find("#"..t) then
+            match = true
+          end
+        end
+      else
+        match = true
+      end
 
-    if err then
-      test_status = { type = "failure", description = description, info = info, trace = stack_trace, err = err }
-    else
-      test_status = { type = "success", description = description, info = info }
-    end
+      local status = { description = context.description, type = "description", run = match }
 
-    if not busted_options.defer_print then
-      output.currently_executing(test_status, busted_options)
-    end
+      if context.setup ~= nil then
+        context.setup()
+      end
 
-    return test_status
-  end
+      for i,v in ipairs(context) do
+        if context.before_each ~= nil then
+          context.before_each()
+        end
 
-  local function run_context(context)
-    local match = false
+        if v.type == "test" then
+          table.insert(status, test(v.description, v.callback))
+        elseif v.type == "describe" then
+          table.insert(status, coroutine.create(function() run_context(v) end))
+        elseif v.type == "pending" then
+          local pending_test_status = { type = "pending", description = v.description, info = v.info }
+          v.callback(pending_test_status)
+          table.insert(status, pending_test_status)
+        end
 
-    if busted_options.tags and #busted_options.tags > 0 then
-      for i,t in ipairs(busted_options.tags) do
-        if context.description:find("#"..t) then
-          match = true
+        if context.after_each ~= nil then
+          context.after_each()
         end
       end
-    else
-      match = true
-    end
 
-    local status = { description = context.description, type = "description", run = match }
-
-    if context.setup ~= nil then
-      context.setup()
-    end
-
-    for i,v in ipairs(context) do
-      if context.before_each ~= nil then
-        context.before_each()
+      if context.teardown ~= nil then
+        context.teardown()
       end
-
-      if v.type == "test" then
-        table.insert(status, test(v.description, v.callback))
-      elseif v.type == "describe" then
-        table.insert(status, coroutine.create(function() run_context(v) end))
-      elseif v.type == "pending" then
-        local pending_test_status = { type = "pending", description = v.description, info = v.info }
-        v.callback(pending_test_status)
-        table.insert(status, pending_test_status)
-      end
-
-      if context.after_each ~= nil then
-        context.after_each()
+      if coroutine.running() then
+        coroutine.yield(status)
+      else
+        return true, status
       end
     end
 
-    if context.teardown ~= nil then
-      context.teardown()
+    local play_sound = function(failures)
+      local failure_messages = {
+        "You have %d busted specs",
+        "Your specs are busted",
+        "Your code is bad and you should feel bad",
+        "Your code is in the Danger Zone",
+        "Strange game. The only way to win is not to test",
+        "My grandmother wrote better specs on a 3 86",
+        "Every time there's a failure, drink another beer",
+        "Feels bad man"
+      }
+
+      local success_messages = {
+        "Aww yeah, passing specs",
+        "Doesn't matter, had specs",
+        "Feels good, man",
+        "Great success",
+        "Tests pass, drink another beer",
+      }
+
+      math.randomseed(os.time())
+
+      if failures > 0 then
+        io.popen("say \""..failure_messages[math.random(1, #failure_messages)]:format(failures).."\"")
+      else
+        io.popen("say \""..success_messages[math.random(1, #success_messages)].."\"")
+      end
     end
-    if coroutine.running() then
-      coroutine.yield(status)
-    else
-      return true, status
+
+    local ms = os.clock()
+
+    if not self.options.defer_print then
+      print(self.output.header(self.root_context))
     end
-  end
 
-  local play_sound = function(failures)
-    local failure_messages = {
-      "You have %d busted specs",
-      "Your specs are busted",
-      "Your code is bad and you should feel bad",
-      "Your code is in the Danger Zone",
-      "Strange game. The only way to win is not to test",
-      "My grandmother wrote better specs on a 3 86",
-      "Every time there's a failure, drink another beer",
-      "Feels bad man"
-    }
-
-    local success_messages = {
-      "Aww yeah, passing specs",
-      "Doesn't matter, had specs",
-      "Feels good, man",
-      "Great success",
-      "Tests pass, drink another beer",
-    }
-
-    math.randomseed(os.time())
-
-    if failures > 0 then
-      io.popen("say \""..failure_messages[math.random(1, #failure_messages)]:format(failures).."\"")
-    else
-      io.popen("say \""..success_messages[math.random(1, #success_messages)].."\"")
-    end
-  end
-
-  local ms = os.clock()
-
-  if not busted_options.defer_print then
-    print(output.header(global_context))
-  end
-
-  local function get_statuses(done, list)
-    local ret = {}
-    for i,v in pairs(list) do
-      local vtype = type(v)
-      if vtype == "thread" then
-        local res = get_statuses(coroutine.resume(v))
-        for key,value in pairs(res) do
-          table.insert(ret, value)
-        end
-      elseif vtype == "table" then
+    --fire off tests, return status list
+    local function get_statuses(done, list)
+      local ret = {}
+      for i,v in pairs(list) do
+        local vtype = type(v)
+        if vtype == "thread" then
+          local res = get_statuses(coroutine.resume(v))
+          for key,value in pairs(res) do
+            table.insert(ret, value)
+          end
+        elseif vtype == "table" then
           table.insert(ret, v)
+        end
       end
+      return ret
     end
-    return ret
+    local statuses = get_statuses(run_context(self.root_context))
+
+    --final run time
+    ms = os.clock() - ms
+
+    if self.options.defer_print then
+      print(self.output.header(self.root_context))
+    end
+
+    local status_string = self.output.formatted_status(statuses, self.options, ms)
+
+    if self.options.sound then
+      play_sound(failures)
+    end
+
+    return status_string
   end
+}
+busted = setmetatable(busted, busted)
 
-  local statuses = get_statuses(run_context(global_context))
-  ms = os.clock() - ms
-
-  if busted_options.defer_print then
-    print(output.header(global_context))
-  end
-
-  local status_string = output.formatted_status(statuses, busted_options, ms)
-
-  if busted_options.sound then
-    play_sound(failures)
-  end
-
-  return status_string
-end
-
--- External functions
-
+-- Global functions
 describe = function(description, callback)
-  local match = current_context.run
+  local match = busted.current_context.run
 
-  if busted_options.tags and #busted_options.tags > 0 then
-    for i,t in ipairs(busted_options.tags) do
+  if busted.options.tags and #busted.options.tags > 0 then
+    for i,t in ipairs(busted.options.tags) do
       if description:find("#"..t) then
         match = true
       end
@@ -186,21 +184,21 @@ describe = function(description, callback)
 
   local local_context = { description = description, callback = callback, type = "describe", run = match  }
 
-  table.insert(current_context, local_context)
+  table.insert(busted.current_context, local_context)
 
-  current_context = local_context
+  busted.current_context = local_context
 
   callback()
 
-  current_context = global_context
+  busted.current_context = busted.root_context
 end
 
 it = function(description, callback)
-  local match = current_context.run
+  local match = busted.current_context.run
 
   if not match then
-    if busted_options.tags and #busted_options.tags > 0 then
-      for i,t in ipairs(busted_options.tags) do
+    if busted.options.tags and #busted.options.tags > 0 then
+      for i,t in ipairs(busted.options.tags) do
         if description:find("#"..t) then
           match = true
         end
@@ -208,8 +206,8 @@ it = function(description, callback)
     end
   end
 
-  if current_context.description ~= nil and match then
-    table.insert(current_context, { description = description, callback = callback, type = "test" })
+  if busted.current_context.description ~= nil and match then
+    table.insert(busted.current_context, { description = description, callback = callback, type = "test" })
   elseif match then
     test(description, callback)
   end
@@ -229,45 +227,29 @@ pending = function(description, callback)
     type = "pending",
     info = info,
     callback = function(self)
-      if not busted_options.defer_print then
-        output.currently_executing(self, busted_options)
+      if not busted.options.defer_print then
+        busted.output.currently_executing(self, busted.options)
       end
     end
   }
 
-  table.insert(current_context, test_status)
+  table.insert(busted.current_context, test_status)
 end
 
 before_each = function(callback)
-  current_context.before_each = callback
+  busted.current_context.before_each = callback
 end
 
 after_each = function(callback)
-  current_context.after_each = callback
+  busted.current_context.after_each = callback
 end
 
 setup = function(callback)
-  current_context.setup = callback
+  busted.current_context.setup = callback
 end
 
 teardown = function(callback)
-  current_context.teardown = callback
-end
-
-set_busted_options = function(options)
-  busted_options = options
-
-  if busted_options.lang then
-    s:set_namespace(busted_options.lang)
-  end
-
-  if busted_options.tags then
-    busted_options.tags = split(busted_options.tags, ",")
-  end
-
-  if options.output_lib then
-    output = require('output.'..options.output_lib)()
-  end
+  busted.current_context.teardown = callback
 end
 
 return busted
