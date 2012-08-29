33108eca84a3c17d6b7a8afc9ea957ba1fb9cee2
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-08-29 19:40:32 +0200

    this assumes that luassert/all.lua will be renamed init.lua and will export the assert command.

--- src/interface.lua
+++ src/interface.lua
@@ -1,5 +1,10 @@
 local current_context = busted.root_context
 
+-- Expose luassert elements as part of global interfcae
+assert = require('luassert')
+spy = require('luassert.spy')
+mock = require('luassert.mock')
+
 -- Global functions
 describe = function(description, callback)
   local match = current_context.run
