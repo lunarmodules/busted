20495b9c4d60a94dbcf89bf4fb640fefbe6e98b3
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-09-01 00:13:04 +0200

    Updated language files, to be correctly split between luassert and busted

--- src/languages/ar.lua
+++ src/languages/ar.lua
@@ -21,24 +21,6 @@ s:set("output.success_single", "نَجاح")
 
 s:set("output.seconds", "ثَوانٍ")
 
-s:set("assertion.same.positive", "تُوُقِّعَ تَماثُلُ الكائِنات. تَمَّ إدخال:\n %s. بَينَما كانَ مِن المُتَوقَّع:\n %s.")
-s:set("assertion.same.negative", "تُوُقِّعَ إختِلافُ الكائِنات. تَمَّ إدخال:\n %s. بَينَما كانَ مِن غَيرِ المُتَوقَّع:\n %s.")
-
-s:set("assertion.equals.positive", "تُوُقِّعَ أن تَتَساوىْ الكائِنات. تمَّ إِدخال:\n %s. بَينَما كانَ من المُتَوقَّع:\n %s.")
-s:set("assertion.equals.negative", "تُوُقِّعَ ألّا تَتَساوىْ الكائِنات. تمَّ إِدخال:\n %s. بَينَما كانَ مِن غير المُتًوقَّع:\n %s.")
-
-s:set("assertion.unique.positive", "تُوُقِّعَ أَنْ يَكونَ الكائِنٌ فَريد: \n%s")
-s:set("assertion.unique.negative", "تُوُقِّعَ أنْ يَكونَ الكائِنٌ غَيرَ فَريد: \n%s")
-
-s:set("assertion.error.positive", "تُوُقِّعَ إصدارُ خطأْ.")
-s:set("assertion.error.negative", "تُوُقِّعَ عدم إصدارِ خطأ.")
-
-s:set("assertion.truthy.positive", "تُوُقِّعَت قيمةٌ صَحيحة، بينما كانت: \n%s")
-s:set("assertion.truthy.negative", "تُوُقِّعَت قيمةٌ غيرُ صَحيحة، بينما كانت: \n%s")
-
-s:set("assertion.falsy.positive", "تُوُقِّعَت قيمةٌ خاطِئة، بَينَما كانت: \n%s")
-s:set("assertion.falsy.negative", "تُوُقِّعَت قيمةٌ غيرُ خاطِئة، بَينَما كانت: \n%s")
-
 -- definitions following are not used within the 'say' namespace but are defined as globals
 failure_messages = {
     "فَشِلَت %d مِنْ الإِختِبارات",
--- src/languages/fr.lua
+++ src/languages/fr.lua
@@ -21,24 +21,7 @@ s:set("output.success_single", "reussite")
 
 s:set("output.seconds", "secondes")
 
-s:set("assertion.same.positive", "Objets supposes de meme nature attendus. Argument passe:\n%s\nAttendu:\n%s")
-s:set("assertion.same.negative", "Objets supposes de natures differentes attendus. Argument passe:\n%s\nNon attendu:\n%s")
-
-s:set("assertion.equals.positive", "Objets supposes etre de valeur egale attendus. Argument passe:\n%s\nAttendu:\n%s")
-s:set("assertion.equals.negative", "Objets supposes etre de valeurs differentes attendu. Argument passe:\n%s\nNon attendu:\n%s")
-
-s:set("assertion.unique.positive", "Objet suppose etre unique attendu:\n%s")
-s:set("assertion.unique.negative", "Objet suppose ne pas etre unique attendu:\n%s")
-
-s:set("assertion.error.positive", "Erreur supposee etre generee.")
-s:set("assertion.error.negative", "Erreur non supposee etre generee.\n%s")
-
-s:set("assertion.truthy.positive", "Assertion supposee etre vraie mais de valeur:\n%s")
-s:set("assertion.truthy.negative", "Assertion supposee etre fausse mais de valeur:\n%s")
-
-s:set("assertion.falsy.positive", "Assertion supposee etre fausse mais de valeur:\n%s")
-s:set("assertion.falsy.negative", "Assertion supposee etre vraie mais de valeur:\n%s")
-
+-- definitions following are not used within the 'say' namespace but are defined as globals
 failure_messages = {
   "Vous avez %d test(s) qui a/ont echoue(s)",
   "Vos tests ont echoue.",
