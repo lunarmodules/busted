9f38101ac3248c30c0ba48f6a321689b0e9e82ae
Gerhard Lipp <gelipp@gmail.com>
2013-02-12 11:44:02 +0100

    restructure to enforce "coroutine cross pcall" issues if using wrong pcall. use setloop interface.

--- spec/copas_spec.lua
+++ spec/copas_spec.lua
@@ -1,48 +1,45 @@
 package.path = './?.lua;'..package.path
 local copas = require'copas'
 local socket = require'socket'
-require'busted'
 
 local port = 19281
 
-local echo_server = copas.addserver(
-   socket.bind('*',port),
-   function(skt)
-      while true do
-         local data = copas.receive(skt)
-         if not data then
-            return
-         end
-         copas.send(skt,data..'\n')               
-      end
-   end)
-
 local echo = function(done)
+   local listener = socket.bind('*',port)
    copas.addthread(
-      function()
-         local s = socket.connect('localhost',port)
-         s:settimeout(0)
-         local client = copas.wrap(s)
-         local msg = 'HALLO'
-         client:send(msg..'\n')
-         client:receive('*l')
-         s:close()
-         done()
-      end)
+      guard(
+         function()
+            local s = socket.tcp()
+            s:setoption('tcp-nodelay',true)
+            copas.connect(s,'localhost',port)
+            local client = copas.wrap(s)
+            local msg = 'HALLO'
+            client:send(msg..'\n')
+            assert(client:receive('*l')==msg)
+            s:close()
+            listener:close()
+            done()
+         end))
+   copas.addserver(
+      listener,
+      guard(
+         function(skt)
+            while true do
+               local data = copas.receive(skt)
+               if not data then
+                  return
+               end
+               copas.send(skt,data..'\n')               
+            end
+         end))
 end
 
 local yield = echo
 
+setloop('copas')
 busted.setup_async_tests(yield,'copas')
 
-local options = {
-   debug = true,
-   loop = function()
-      copas.step(0)
-   end
-} 
-
-local statuses = busted.run(options)
+local statuses = busted.run{debug=true}
 
 busted.reset()
 busted.describe_statuses(statuses)
