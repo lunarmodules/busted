aee96dc67811cce904bb08f1d66b1616a4d7cb15
Thijs Schreijer <thijs@thijsschreijer.nl>
2013-05-17 17:20:37 +0200

    fixed cl spec to run on other platforms

--- spec/cl_test.lua
+++ spec/cl_test.lua
@@ -3,15 +3,17 @@
 --
 --    busted --pattern=cl_test.lua --defer-print
 
-
+local ditch
 local error_started
 local error_start = function()
+  if ditch == "" then return end
   print("================================================")
   print("==  Error block follows                       ==")
   print("================================================")
   error_started = true
 end
 local error_end = function()
+  if ditch == "" then return end
   print("================================================")
   print("==  Error block ended, all according to plan  ==")
   print("================================================")
@@ -26,6 +28,16 @@ local modexit = function(exitcode)
     return exitcode
   end
 end
+
+local path = require("pl.path")
+local = " > /dev/null 2>&1"
+if path.is_windows then
+  ditch = " 1> NUL 2>NUL"
+end
+
+--ditch = ""  -- uncomment this line, to show output of failing commands, for debugging
+
+
 
 
 describe("Tests the busted command-line options", function()
@@ -47,13 +59,13 @@ describe("Tests the busted command-line options", function()
   it("tests running with --tags specified", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=_tags.lua$")
+    success, exitcode = utils.execute("busted --pattern=_tags.lua$"..ditch)
     assert.is_false(success)
     assert.is_equal(modexit(exitcode), 3)
-    success, exitcode = utils.execute("busted --pattern=_tags.lua$ --tags=tag1")
+    success, exitcode = utils.execute("busted --pattern=_tags.lua$ --tags=tag1"..ditch)
     assert.is_false(success)
     assert.is_equal(modexit(exitcode), 2)
-    success, exitcode = utils.execute("busted --pattern=_tags.lua$ --tags=tag1,tag2")
+    success, exitcode = utils.execute("busted --pattern=_tags.lua$ --tags=tag1,tag2"..ditch)
     assert.is_false(success)
     assert.is_equal(modexit(exitcode), 3)
     error_end()
@@ -62,10 +74,10 @@ describe("Tests the busted command-line options", function()
   it("tests running with --lang specified", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=cl_success.lua$ --lang=en")
+    success, exitcode = utils.execute("busted --pattern=cl_success.lua$ --lang=en"..ditch)
     assert.is_true(success)
     assert.is_equal(modexit(exitcode), 0)  
-    success, exitcode = utils.execute("busted --pattern=cl_success --lang=not_found_here")
+    success, exitcode = utils.execute("busted --pattern=cl_success --lang=not_found_here"..ditch)
     assert.is_false(success)
     assert.is_equal(modexit(exitcode), 1)  -- busted errors out on non-available language
     error_end()
@@ -73,14 +85,14 @@ describe("Tests the busted command-line options", function()
 
   it("tests running with --version specified", function()
     local success, exitcode
-    success, exitcode = utils.execute("busted --version")
+    success, exitcode = utils.execute("busted --version"..ditch)
     assert.is_true(success)
     assert.is_equal(modexit(exitcode), 0)
   end)
 
   it("tests running with --help specified", function()
     local success, exitcode
-    success, exitcode = utils.execute("busted --help")
+    success, exitcode = utils.execute("busted --help"..ditch)
     assert.is_true(success)
     assert.is_equal(modexit(exitcode), 0)
   end)
@@ -88,7 +100,7 @@ describe("Tests the busted command-line options", function()
   it("tests running a non-compiling testfile", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=cl_compile_fail.lua$")
+    success, exitcode = utils.execute("busted --pattern=cl_compile_fail.lua$"..ditch)
     assert.is_false(success)
     assert.is_equal(modexit(exitcode), 1)
     error_end()
@@ -97,7 +109,7 @@ describe("Tests the busted command-line options", function()
   it("tests running a testfile throwing errors when being run", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=cl_execute_fail.lua$")
+    success, exitcode = utils.execute("busted --pattern=cl_execute_fail.lua$"..ditch)
     assert.is_false(success)
     assert.is_equal(modexit(exitcode), 1)
     error_end()
@@ -106,10 +118,10 @@ describe("Tests the busted command-line options", function()
   it("tests running with --output specified", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=cl_success.lua$ --output=TAP")
+    success, exitcode = utils.execute("busted --pattern=cl_success.lua$ --output=TAP"..ditch)
     assert.is_true(success)
     assert.is_equal(modexit(exitcode), 0)  
-    success, exitcode = utils.execute("busted --pattern=cl_two_failures.lua$ --output=not_found_here")
+    success, exitcode = utils.execute("busted --pattern=cl_two_failures.lua$ --output=not_found_here"..ditch)
     assert.is_false(success)
     assert.is_equal(modexit(exitcode), 1)  -- 1 for outputter missing
     error_end()
