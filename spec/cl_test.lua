952c476416742e38a5fb2c4513b752c0338c9dd6
Thijs Schreijer <thijs@thijsschreijer.nl>
2013-05-17 10:03:01 +0200

    fixed command line tests (cl_xxx files)

--- spec/cl_test.lua
+++ spec/cl_test.lua
@@ -1,3 +1,9 @@
+-- Tests the commandline options by executing busted through
+-- os.execute(). It can be run through the following command:
+--
+--    busted --pattern=cl_test.lua --defer-print
+
+
 local error_started
 local error_start = function()
   print("================================================")
@@ -32,13 +38,13 @@ describe("Tests the busted command-line options", function()
   it("tests running with --tags specified", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=_test.lua$")
+    success, exitcode = utils.execute("busted --pattern=_tags.lua$")
     assert.is_false(success)
     assert.is_equal(exitcode, 3)
-    success, exitcode = utils.execute("busted --pattern=_test.lua$ --tags=tag1")
+    success, exitcode = utils.execute("busted --pattern=_tags.lua$ --tags=tag1")
     assert.is_false(success)
     assert.is_equal(exitcode, 2)
-    success, exitcode = utils.execute("busted --pattern=_test.lua$ --tags=tag1,tag2")
+    success, exitcode = utils.execute("busted --pattern=_tags.lua$ --tags=tag1,tag2")
     assert.is_false(success)
     assert.is_equal(exitcode, 3)
     error_end()
@@ -47,10 +53,10 @@ describe("Tests the busted command-line options", function()
   it("tests running with --lang specified", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=_test.lua$ --tags=tag3 --lang=en")
+    success, exitcode = utils.execute("busted --pattern=cl_success.lua$ --lang=en")
     assert.is_true(success)
     assert.is_equal(exitcode, 0)  
-    success, exitcode = utils.execute("busted --pattern=_test.lua$ --tags=tag3 --lang=not_found_here")
+    success, exitcode = utils.execute("busted --pattern=cl_success --lang=not_found_here")
     assert.is_false(success)
     assert.is_equal(exitcode, 1)  -- busted errors out on non-available language
     error_end()
@@ -70,17 +76,10 @@ describe("Tests the busted command-line options", function()
     assert.is_equal(exitcode, 0)
   end)
 
-  it("tests running with --output=TAP specified", function()
-    local success, exitcode
-    success, exitcode = utils.execute("busted --pattern=_test.lua$ --tags=tag3 --output=TAP")
-    assert.is_true(success)
-    assert.is_equal(exitcode, 0)
-  end)
-
   it("tests running a non-compiling testfile", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=compile_fail.lua$")
+    success, exitcode = utils.execute("busted --pattern=cl_compile_fail.lua$")
     assert.is_false(success)
     assert.is_equal(exitcode, 1)
     error_end()
@@ -89,7 +88,7 @@ describe("Tests the busted command-line options", function()
   it("tests running a testfile throwing errors when being run", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=execute_fail.lua$")
+    success, exitcode = utils.execute("busted --pattern=cl_execute_fail.lua$")
     assert.is_false(success)
     assert.is_equal(exitcode, 1)
     error_end()
@@ -98,12 +97,12 @@ describe("Tests the busted command-line options", function()
   it("tests running with --output specified", function()
     local success, exitcode
     error_start()
-    success, exitcode = utils.execute("busted --pattern=_test.lua$ --tags=tag3 --output=TAP")
+    success, exitcode = utils.execute("busted --pattern=cl_success.lua$ --output=TAP")
     assert.is_true(success)
     assert.is_equal(exitcode, 0)  
-    success, exitcode = utils.execute("busted --pattern=_test.lua$ --tags=tag1 --output=not_found_here")
+    success, exitcode = utils.execute("busted --pattern=cl_two_failures.lua$ --output=not_found_here")
     assert.is_false(success)
-    assert.is_equal(exitcode, 3)  -- tag1 has 2, +1 for outputter missing
+    assert.is_equal(exitcode, 1)  -- 1 for outputter missing
     error_end()
   end)
 
