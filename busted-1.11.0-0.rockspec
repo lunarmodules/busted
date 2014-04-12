2ded1f55a2314cee25d4a5689e34e7f92e47ac46
Jack Lawson <jack@jack.local>
2014-04-12 09:37:20 -0700

    version 1.11.0

--- busted-1.11.0-0.rockspec
+++ busted-1.11.0-0.rockspec
@@ -1,8 +1,8 @@
 package = "busted"
-version = "1.10.0-0"
+version = "1.11.0-0"
 source = {
-  url = "https://github.com/Olivine-Labs/busted/archive/v1.10.0.tar.gz",
-  dir = "busted-1.10.0"
+  url = "https://github.com/Olivine-Labs/busted/archive/v1.11.0.tar.gz",
+  dir = "busted-1.11.0"
 }
 description = {
   summary = "Elegant Lua unit testing.",
--- src/core.lua
+++ src/core.lua
@@ -10,9 +10,9 @@ settimeout = nil
 
 -- exported module table
 local busted = {}
-busted._COPYRIGHT   = "Copyright (c) 2013 Olivine Labs, LLC."
+busted._COPYRIGHT   = "Copyright (c) 2014 Olivine Labs, LLC."
 busted._DESCRIPTION = "A unit testing framework with a focus on being easy to use. http://www.olivinelabs.com/busted"
-busted._VERSION     = "Busted 1.10.0"
+busted._VERSION     = "Busted 1.11.0"
 
 -- set defaults
 busted.defaultoutput = path.is_windows and "plain_terminal" or "utf_terminal"
