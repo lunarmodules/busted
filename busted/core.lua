b5a8993dce92be11bec78729e7632e7a85aa4dfd
Jack Lawson <ajacksified@gmail.com>
2014-12-31 07:39:12 -0800

    v2.0.rc5

--- busted/core.lua
+++ busted/core.lua
@@ -31,7 +31,7 @@ return function()
   local mediator = require 'mediator'()
 
   local busted = {}
-  busted.version = '2.0.rc3-0'
+  busted.version = '2.0.rc5-0'
 
   local root = require 'busted.context'()
   busted.context = root.ref()
