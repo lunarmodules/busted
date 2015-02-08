1b08c8b6b587efea355de2738eef64b2f8f6950f
Oscar Lim <olim@ucla.edu>
2015-02-07 16:13:49 -0800

    Use `wget` in travis setup
    Switch to using `wget` instead of `curl` in travis setup so that site
    redirection will work when downloading source packages to install.

--- .travis_setup.sh
+++ .travis_setup.sh
@@ -9,22 +9,22 @@ echo 'rocks_servers = {
 
 
 if [ "$LUA" == "LuaJIT 2.0" ]; then
-  curl http://luajit.org/download/LuaJIT-2.0.2.tar.gz | tar xz
+  wget -O - http://luajit.org/download/LuaJIT-2.0.2.tar.gz | tar xz
   cd LuaJIT-2.0.2
   make && sudo make install INSTALL_TSYMNAME=lua;
 else
   if [ "$LUA" == "Lua 5.1" ]; then
-    curl http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
+    wget -O - http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
     cd lua-5.1.5;
   elif [ "$LUA" == "Lua 5.2" ]; then
-    curl http://www.lua.org/ftp/lua-5.2.3.tar.gz | tar xz
+    wget -O - http://www.lua.org/ftp/lua-5.2.3.tar.gz | tar xz
     cd lua-5.2.3;
   fi
   sudo make linux install;
 fi
 
 cd ..
-curl http://luarocks.org/releases/luarocks-2.1.2.tar.gz | tar xz
+wget -O - http://luarocks.org/releases/luarocks-2.1.2.tar.gz | tar xz
 cd luarocks-2.1.2
 
 if [ "$LUA" == "LuaJIT 2.0" ]; then
