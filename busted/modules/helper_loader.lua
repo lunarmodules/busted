0cb016ffb79babca2cf1ac78d3dbe4b075bcf0a2
Oscar Lim <olim@ucla.edu>
2015-02-18 15:59:32 -0800

    Add a helper loader module

---
+++ busted/modules/helper_loader.lua
@@ -0,0 +1,19 @@
+local utils = require 'busted.utils'
+
+return function()
+  local loadHelper = function(helper, hpath, options)
+    local success, err = pcall(function()
+      if helper:match('%.lua$') or helper:match('%.moon$') then
+        dofile(utils.normpath(hpath))
+      else
+        require(helper)
+      end
+    end)
+
+    if not success then
+      busted.publish({ 'error', 'helper' }, { descriptor = 'helper', name = helper }, nil, err, {})
+    end
+  end
+
+  return loadHelper
+end
