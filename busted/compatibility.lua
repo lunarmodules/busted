cd53b799332d933f28b93f3b29d6046783afc970
Boris Nagaev <bnagaev@gmail.com>
2015-02-24 02:24:18 +0300

    fix memoryleak in userdata on os.exit in Lua 5.1
    Not calling os.exit results in running a code
    twice in standalone mode.
    
    Function compatibility.osexit runs collectgarbage()
    while value of collectgarbage('count')
    (which is memory usage) changes.

--- busted/compatibility.lua
+++ busted/compatibility.lua
@@ -30,5 +30,31 @@ return {
     if f ~= 0 then return f end
   end,
 
-  unpack = table.unpack or unpack
+  unpack = table.unpack or unpack,
+
+  osexit = function(code, close)
+    if close and _VERSION == 'Lua 5.1' then
+      -- From Lua 5.1 manual:
+      -- > The userdata itself is freed only in the next
+      -- > garbage-collection cycle.
+      -- Call collectgarbage() while collectgarbage('count')
+      -- changes + 3 times, at least 3 times,
+      -- at max 100 times (to prevent infinite loop).
+      local times_const = 0
+      for i = 1, 100 do
+        local count_before = collectgarbage("count")
+        collectgarbage()
+        local count_after = collectgarbage("count")
+        if count_after == count_before then
+          times_const = times_const + 1
+          if times_const > 3 then
+            break
+          end
+        else
+          times_const = 0
+        end
+      end
+    end
+    os.exit(code, close)
+  end,
 }
