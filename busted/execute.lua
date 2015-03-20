c3ee7ba4ce177be98192e45f6a714992901b4a90
Oscar Lim <olim@ucla.edu>
2015-03-20 15:07:08 -0700

    Add a gtest style output handler
    This outputs the test name before a test is run and outputs any
    failures/errors as they are encountered. The `--defer-print` option can
    be used to defer the output of failures/errors to the end of the test
    suite.

--- busted/execute.lua
+++ busted/execute.lua
@@ -31,7 +31,7 @@ return function(busted)
       end
 
       local root = busted.context.get()
-      busted.publish({ 'suite', 'start' }, i, runs)
+      busted.publish({ 'suite', 'start' }, i, runs, busted.randomize and busted.randomseed or nil)
       if block.setup(root) then
         busted.execute()
       end
