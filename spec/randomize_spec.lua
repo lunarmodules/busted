719d93b6dfe7aea09d96c59736784800fcd93a28
Oscar Lim <olim@ucla.edu>
2015-03-26 13:47:34 -0700

    Support disabling randomization in context blocks
    This adds a `norandomize` function to file and describe contexts so that
    test randomization within the current context block can be explicitly
    disabled, overriding any command-line flags to enable randomization.

--- spec/randomize_spec.lua
+++ spec/randomize_spec.lua
@@ -18,3 +18,23 @@ describe('Order of tests ran', function()
     assert.are_not.same(unexpected, order)
   end)
 end)
+
+describe('Disabling randomized test order', function()
+  randomize()
+  norandomize()
+
+  local expected = {}
+  local order = {}
+
+  for i = 1, 100 do
+    table.insert(expected, i)
+
+    it('does 100 its', function()
+      table.insert(order, i)
+    end)
+  end
+
+  it('does not randomize tests', function()
+    assert.are.same(expected, order)
+  end)
+end)
