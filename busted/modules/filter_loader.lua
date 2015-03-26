607db71bc01435da4c547c39b22226c8e0391b08
Oscar Lim <olim@ucla.edu>
2015-03-25 22:08:07 -0700

    Fix --no-keep-going to skip support functions
    If the --no-keep-going flag is set, then skip support functions after an
    error is encountered.

--- busted/modules/filter_loader.lua
+++ busted/modules/filter_loader.lua
@@ -70,7 +70,7 @@ return function()
     end
 
     local skipOnError = function()
-      return nil, not busted.skippAll
+      return nil, not busted.skipAll
     end
 
     local applyFilter = function(descriptors, name, fn)
