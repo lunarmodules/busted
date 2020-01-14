8ca192d0ba5d65b4086466dc4d389b688cfdf8d1
Open Skies <alexjiverson@gmail.com>
2020-01-14 06:35:02 -0600

    Update terra loader to use the global-injection of current Terra (#606)

--- busted/modules/files/terra.lua
+++ busted/modules/files/terra.lua
@@ -1,7 +1,10 @@
 local path = require 'pl.path'
 
 local ret = {}
-local ok, terralib = pcall(function() return require 'terralib' end)
+local terra_available, terralib = not not terralib, terralib --grab the injected global if it exists
+if not terra_available then
+  terra_available, terralib = pcall(require, 'terra') --otherwise, attempt to load terra as a shared library
+end
 
 local getTrace = function(filename, info)
   local index = info.traceback:find('\n%s*%[C]')
@@ -10,15 +13,19 @@ local getTrace = function(filename, info)
 end
 
 ret.match = function(busted, filename)
-  return ok and path.extension(filename) == '.t'
+  return path.extension(filename) == '.t'
 end
 
 ret.load = function(busted, filename)
-  local file, err = terralib.loadfile(filename)
-  if not file then
-    busted.publish({ 'error', 'file' }, { descriptor = 'file', name = filename }, nil, err, {})
+  if not terra_available then
+    error "unable to load terra, try running without terra language support enabled or installing terra."
+  else
+    local file, err = terralib.loadfile(filename)
+    if not file then
+      busted.publish({ 'error', 'file' }, { descriptor = 'file', name = filename }, nil, err, {})
+    end
+    return file, getTrace
   end
-  return file, getTrace
 end
 
 return ret
