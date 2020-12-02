278356adecbdbbd4aa316ff51bc0dcbc6a329260
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-12-02 09:52:29 +0100

    chore(lint) fix lint issues

--- spec/fixtures_spec.lua
+++ spec/fixtures_spec.lua
@@ -1,16 +1,14 @@
-local pl_path = require "pl.path"
-
 describe("fixtures:", function()
 
   describe("fixture_path()", function()
 
     it("returns the absolute fixture path", function()
-      local path = fixture_path("fixtures/myfile.txt")
+      local path = fixture_path("fixtures/myfile.txt") -- luacheck: ignore
       assert.match("^.-busted[/\\]spec[/\\]fixtures[/\\]myfile.txt$", path)
     end)
 
     it("returns the absolute fixture path normalized", function()
-      local path = fixture_path("../fixtures/myfile.txt")
+      local path = fixture_path("../fixtures/myfile.txt") -- luacheck: ignore
       assert.match("^.-busted[/\\]fixtures[/\\]myfile.txt$", path)
     end)
 
